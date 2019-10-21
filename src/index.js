642875788fedd7b84b72e3dd06e2f9298efe4fe5
unknown <skarolis99@gmail.com>
2019-10-21 19:04:34 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './assets/style.scss'
+
+// import Icon from './assets/img/unnamed.png';
+
+// const img = new Image()
+// img.src = Icon
+
+
+// const div = document.createElement('div')
+// document.querySelector('body').appendChild(div);
+// document.querySelector('div').appendChild(img);
